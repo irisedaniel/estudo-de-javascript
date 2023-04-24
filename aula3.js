4b2f73555bef5610980851b4f0f043bf498426c3
irisedaniel <128431551+irisedaniel@users.noreply.github.com>
2023-04-24 20:53:17 -0300

    aula3.js
    aula3.js

---
+++ aula3.js
@@ -0,0 +1,11 @@
+var nome = prompt("informe seu nome");
+var nascimento = prompt("informe o ano que voce nasceu");
+var idade = 2023 - nascimento;
+if(idade >= 18  ){
+     alert("ola seu nome e " + nome + " e sua idade " + idade + " voce e maior de idade");
+ }
+ 
+ else{
+    alert("ola seu nome e " + nome + " e sua idade " + idade + " voce e menor de idade");
+ }
+
